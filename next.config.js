b623cb1cfcd76ca6fca6fc53a5755dbdb281e5c5
Ewerton G <tonpublic97@gmail.com>
2023-11-13 02:14:37 -0300

    fix image server error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+        domains: ['i.imgur.com', '*', 'outro-dominio.com', 'mais-um-dominio.com'],
+    },
+    // adicione os dominios que forem ser utilizados para as imagens caso dê erro.
+  }
+  
+  module.exports = nextConfig
+  
