21b64df2e93080495118291b31931d96caeddc4c
Ewerton G <tonpublic97@gmail.com>
2023-11-13 17:27:36 -0300

    Alter server image config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['i.imgur.com', '*', 'outro-dominio.com', 'mais-um-dominio.com'],
+        domains: ['i.imgur.com', '*', 'media.licdn.com', 'localhost', '127.0.0.1'],
     },
     // adicione os dominios que forem ser utilizados para as imagens caso dê erro.
   }
